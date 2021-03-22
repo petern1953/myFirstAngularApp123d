470459c38b97e20fb577736fbd593ca129445030
petern1953 <petern1953@gmail.com>
2021-03-22 08:11:20 +0100

    hero comp ??? kódok

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+// ********** utólagos beszúrás
+import { ActivatedRoute, RouterState } from '@angular/router';
+// **********
 import { Observable } from 'rxjs';
 import { Hero } from '../model/hero';
 import { HeroService } from '../service/hero.service';
@@ -7,20 +10,36 @@ import { HeroService } from '../service/hero.service';
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
-  styleUrls: ['./hero.component.scss']
+  styleUrls: ['./hero.component.scss'],
 })
 export class HeroComponent implements OnInit {
-  heroList: Observable<Hero[]> = new Observable;
+  heroList: Observable<Hero[]> = new Observable();
 
   constructor(
     private hService: HeroService,
-    private router: Router
-  ) {
+    private router: Router,
+    // ********** utólagos beszúrás
+    route: ActivatedRoute
+  ) // ********************
+  {
     this.heroList = this.hService.getAll();
+    // ********** utólagos beszúrás
+    const state: RouterState = this.router.routerState;
+    console.log('state: ', state);
+    const root: ActivatedRoute = state.root;
+    console.log('root: ', root);
+    const child = root.firstChild;
+    console.log('child: ', child);
+    // innentől nem működik egyik sem
+    // const id: Observable<string> = child?.params.forEach(p => p.id);
+    // console.log("id: ", id);
+    // const id: Observable<string> = route.params.map(p => p.id);
+    // const url: Observable<string> = route.url.map(segments => segments.join(''));
+    // const user = route.data.map(d => d.user);
+    // **************
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   jumpToHero(hero: Hero): void {
     this.router.navigateByUrl(`/hero/${hero.id}`);
